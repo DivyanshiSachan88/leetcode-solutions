8dbcd8fd040fb97589f2f11995829318967d122a
Divyanshi Sachan <divyanshisachan67@gmail.com>
2026-07-03 16:19:30 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (34.05%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        Boolean[][] memo = new Boolean[s.length() + 1][p.length() + 1];
+        return dfs(0, 0, s, p, memo);
+    }
+
+    private boolean dfs(int i, int j, String s, String p, Boolean[][] memo) {
+        if (memo[i][j] != null) return memo[i][j];
+
+        if (j == p.length()) {
+            return memo[i][j] = (i == s.length());
+        }
+
+        boolean firstMatch = (i < s.length()) &&
+                (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            memo[i][j] = dfs(i, j + 2, s, p, memo) ||
+                         (firstMatch && dfs(i + 1, j, s, p, memo));
+        } else {
+            memo[i][j] = firstMatch && dfs(i + 1, j + 1, s, p, memo);
+        }
+
+        return memo[i][j];
+    }
+}
